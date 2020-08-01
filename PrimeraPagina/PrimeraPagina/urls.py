8b48f280961466d0ed8dce2677b2f5f5f01f3e0f
BrianMarquez3 <brian3marquez@gmail.com>
2020-07-31 21:02:09 -0500

    Primera Pagina Update

--- PrimeraPagina/PrimeraPagina/urls.py
+++ PrimeraPagina/PrimeraPagina/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from PrimeraPagina.views import saludo
+from PrimeraPagina.views import saludo, despedida
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('nosvemos/', despedida),
+
 ]
 
 
--- PrimeraPagina/PrimeraPagina/views.py
+++ PrimeraPagina/PrimeraPagina/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Primera Vista
 def saludo (request):
     return HttpResponse("Hola Github esta es nuestra primera pagina con django")
+
+def despedida (request):
+    return HttpResponse("Hasta luego Github")
