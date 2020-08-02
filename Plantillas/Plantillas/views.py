a3a98e1b3ed4d3ec4b6b4977a7d1112ac287a3ca
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-01 20:10:27 -0500

    template

--- Plantillas/Plantillas/views.py
+++ Plantillas/Plantillas/views.py
@@ -3,15 +3,20 @@
 
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
 
-def saludo (request):
+def saludo (request): # primera vista
 
-    doc_externo.open("C:/Users/brian/Documents/Python-Django/Plantillas/Plantillas/plantillas")
-
+    doc_externo=open("C:/Users/brian/Documents/Python-Django/Plantillas/template/myTemplate.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx=Context()
+    documento=plt.render(ctx)
     return HttpResponse(documento)
 
 def despedida (request):
     return HttpResponse("Hasta luego Github")
+
 
 
 # Funcion que muestra la fecha Actual
