48e8e989d0fef3c8c3eba0d9bec067e6747cdcda
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-10 23:33:24 -0500

    Template V

--- PlantillasIV/PlantillasIV/views.py
+++ PlantillasIV/PlantillasIV/views.py
@@ -65,4 +65,3 @@ def calculaEdad(request, agno, edad):
 
 
 
-
--- PlantillasV/PlantillasV/views.py
+++ PlantillasV/PlantillasV/views.py
@@ -1,9 +1,12 @@
-# Plantillas V Plantillas Incrustadas
+# Plantillas V
+# Simplificacion de codigo con paquete shortcuts
+# Plantillas incrustadas (PLantillas dentro de Plantillas)
 
 from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 from django.template import loader
+from django.shortcuts import render
 
 class Persona(object):
 
@@ -19,15 +22,7 @@ def saludo (request): # primera vista
     temasDelCurso = ["Plantillas", "Modelos", "Formularios", "Vistas", "despliegue"]
     ahora=datetime.datetime.now()
 
-    # CARGADORES DE PLANTILLA
-    #doc_externo = open("C:/Users/brian/Documents/Python-Django/PlantillasIV/template/myTemplate.html")
-    #plt = Template(doc_externo.read())
-    #doc_externo.close()
-
-    doc_externo = loader.get_template('myTemplate.html')
-
-    documento=doc_externo.render({"nombre_persona":p1.nombre, "apellido_persona": p1.apellido, "momento_actual": ahora, "temas":temasDelCurso}) 
-    return HttpResponse(documento)
+    return render(request, "myTemplate.html",{"nombre_persona":p1.nombre, "apellido_persona": p1.apellido, "momento_actual": ahora, "temas":temasDelCurso})
 
 def despedida (request):
     return HttpResponse("Hasta luego Github")
