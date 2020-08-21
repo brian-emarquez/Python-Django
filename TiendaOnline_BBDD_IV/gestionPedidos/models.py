5b54f2423dbf32168235a3bdd3e0786ba6521924
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-21 14:36:15 -0500

    TiendaOnline BBDD IV Start

---
+++ TiendaOnline_BBDD_IV/gestionPedidos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    tfno=models.CharField(max_length=7)
+
+class Articulos(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.CharField(max_length=20)
+    precio=models.IntegerField()
+
+class Pedidos(models.Model):
+    nombre=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
