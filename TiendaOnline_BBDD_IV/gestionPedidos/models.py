d6ddbf8b88bcc4c3a074fd6f75cf795f258db9a2
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-21 18:03:26 -0500

    Tienda Online BBDD IV Update 1

--- TiendaOnline_BBDD_IV/gestionPedidos/models.py
+++ TiendaOnline_BBDD_IV/gestionPedidos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# Identacion
 
 
 class Clientes(models.Model):
@@ -13,8 +14,17 @@ class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
     seccion=models.CharField(max_length=20)
     precio=models.IntegerField()
+    
+    def __str__(self): # visualizar los elemtnos de la base de datos
+        return 'el nombres es %s la se seccion es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
 
 class Pedidos(models.Model):
     nombre=models.IntegerField()
     fecha=models.DateField()
     entregado=models.BooleanField()
+
+# SHELL
+
+# Articulos.objects.filter(seccion='decoracion', nombre='lampara')
+# Articulos.objects.filter(seccion='decoracion', nombre='mesa')  mayor 
+# Articulos.objects.filter(seccion='deportes', precio__lte=100) menor
