fae4c44f359c431d31fd40fae7d7d090c6f51124
BrianMarquez3 <brian3marquez@gmail.com>
2020-08-26 21:42:41 -0500

    Update database

--- TiendaOnline_BBDD_V/TiendaOnline_BBDD_V/settings.py
+++ TiendaOnline_BBDD_V/TiendaOnline_BBDD_V/settings.py
@@ -74,6 +74,19 @@ WSGI_APPLICATION = 'TiendaOnline_BBDD_V.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'articulosclientes',
+        'USER': 'briandb',
+        'PASSWORD': 'briandb',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT': '5432',
+    }
+}
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -84,6 +97,7 @@ DATABASES = {
         'PORT': 3307,
     }
 }
+'''
 
 
 
