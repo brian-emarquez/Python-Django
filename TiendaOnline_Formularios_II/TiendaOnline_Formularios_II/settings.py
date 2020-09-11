9fa76c6038ac38d6cbd8becb28e7d89085fd78b2
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 13:09:13 -0500

    Correcciones

--- TiendaOnline_Formularios_II/TiendaOnline_Formularios_II/settings.py
+++ TiendaOnline_Formularios_II/TiendaOnline_Formularios_II/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kox6&7kh-xd6v2=5$ovmnr+d6pp0qs+7^*9ki99ic=h#8wk2#4'
+SECRET_KEY = 'i$36+2c#_x@8h*wf*+w3831p7w=^v4e1@i%!8e@@%3#kdyart0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestionPedidos',
 ]
 
 MIDDLEWARE = [
@@ -74,27 +73,13 @@ WSGI_APPLICATION = 'TiendaOnline_Formularios_II.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'articuloclientes',
-#        'USER': 'briandb',
-#        'PASSWORD': 'briandb',
-#        'HOST': '127.0.0.1',
-#        'DATABASE_PORT':'5432',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'articulosclientes',
-        'USER': 'briandb',
-        'PASSWORD': 'briandb',
-        'HOST': '127.0.0.1',
-        'PORT': 3307,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
