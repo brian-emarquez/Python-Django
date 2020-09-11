3bd3ec93159fe328e129f099f190ba80a958062b
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-11 14:02:54 -0500

    TiendaOnline_Formularios_II complete

--- TiendaOnline_Formularios_II/TiendaOnline_Formularios_II/settings.py
+++ TiendaOnline_Formularios_II/TiendaOnline_Formularios_II/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestionPedidos',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,25 @@ WSGI_APPLICATION = 'TiendaOnline_Formularios_II.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'articuloclientes',
+#        'USER': 'briandb',
+#        'PASSWORD': 'briandb',
+#        'HOST': '127.0.0.1',
+#        'DATABASE_PORT':'5432',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'articulosclientes',
+        'USER': 'briandb',
+        'PASSWORD': 'briandb',
+        'HOST': '127.0.0.1',
+        'PORT': 3307,
     }
 }
 
--- TiendaOnline_Formularios_II/TiendaOnline_Formularios_II/urls.py
+++ TiendaOnline_Formularios_II/TiendaOnline_Formularios_II/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestionPedidos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda_productos/', views.busqueda_productos),
+    path('buscar/', views.buscar),
 ]
