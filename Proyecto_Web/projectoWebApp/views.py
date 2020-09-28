7d9bda14ceb6f4f13a5aef4cb12f1db83d50ca15
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-28 16:23:29 -0500

    Proyecto Web

---
+++ Proyecto_Web/projectoWebApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Home")
+
+def servivios(request):
+    return HttpResponse("Servicios")
+
+def tienda(request):
+    return HttpResponse("Tienda")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def contacto(request):
+    return HttpResponse("Contacto")
+
+
+
+
