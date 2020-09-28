63b5728fb53fc13a84fa5a6c78c50587febc2e19
BrianMarquez3 <brian3marquez@gmail.com>
2020-09-28 16:31:57 -0500

    Proyecto Web II update

--- Proyecto_Web_II/Proyecto_Web_II/urls.py
+++ Proyecto_Web_II/Proyecto_Web_II/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from projectoWebApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="Home"),
+    path('servivios', views.servivios, name="Servivios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto"),
+
 ]
--- Proyecto_Web_II/projectoWebApp/views.py
+++ Proyecto_Web_II/projectoWebApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home")
+
+def servivios(request):
+    return HttpResponse("Servicios")
+
+def tienda(request):
+    return HttpResponse("Tienda")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def contacto(request):
+    return HttpResponse("Contacto")
