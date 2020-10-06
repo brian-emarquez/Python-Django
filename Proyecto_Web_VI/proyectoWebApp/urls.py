bfa24c9418f793dc34662b67bcbb9ee6cf6e5f56
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-05 20:58:30 -0500

    Proyecto Web VI update

---
+++ Proyecto_Web_VI/proyectoWebApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from proyectoWebApp import views
+
+urlpatterns = [
+
+    path('', views.home, name="Home"),
+    path('servicios', views.servicios, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto"),
+
+]
--- Proyecto_Web_VI/proyectoWebApp/views.py
+++ Proyecto_Web_VI/proyectoWebApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, "proyectoWebApp/home.html")
+
+def servicios(request):
+    return render(request, "proyectoWebApp/servicios.html")
+
+def tienda(request):
+    return render(request, "proyectoWebApp/tienda.html")
+
+def blog(request):
+    return render(request, "proyectoWebApp/blog.html")
+
+def contacto(request):
+    return render(request, "proyectoWebApp/contacto.html")
