1ef6bbb4ff0fa7f192d6c2c0ce92e7aa3934683a
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 22:50:18 -0500

    feat # Proyecto Web IX complete
    Proyecto Web IX complete
    
    resolvesr: #
    se also: #

--- Proyecto_Web_IX/proyectoWebApp/views.py
+++ Proyecto_Web_IX/proyectoWebApp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
+from servicios.models import Servicio 
+
+
 # Create your views here.
 
 def home(request):
     return render(request, "proyectoWebApp/home.html")
 
 def servicios(request):
-    return render(request, "proyectoWebApp/servicios.html")
+
+    # importar todos lo servicios
+    servicios=Servicio.objects.all()
+    return render(request, "proyectoWebApp/servicios.html", {"servicios":servicios})
 
 def tienda(request):
     return render(request, "proyectoWebApp/tienda.html")
