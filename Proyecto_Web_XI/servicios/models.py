7d408d9a7971710b24d66fd12c3520582f50633a
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-08 11:45:38 -0500

    feat # Proyecto Web XI complete
    Proyecto Web X complete
    
    resolvesr: #
    se also: #

--- Proyecto_Web_XI/servicios/models.py
+++ Proyecto_Web_XI/servicios/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Servicio(models.Model):
     titulo=models.CharField(max_length=50)
-    contenido=models.CharField(max_length=50)
+    contenido=models.CharField(max_length=100)
     imagen=models.ImageField(upload_to='servicios')
     created=models.DateTimeField(auto_now_add=True)
     update=models.DateTimeField(auto_now_add=True)
