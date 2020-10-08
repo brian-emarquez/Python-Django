ec645f413accfc527f36c466b370e47f9cae8f9b
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 21:36:46 -0500

    Update 2

--- Proyecto_Web_IX/servicios/admin.py
+++ Proyecto_Web_IX/servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Servicio
 
 # Register your models here.
+
+class ServicioAdmin(admin.ModelAdmin):
+    readonly_fields=("created", "update")
+
+admin.site.register(Servicio, ServicioAdmin)
