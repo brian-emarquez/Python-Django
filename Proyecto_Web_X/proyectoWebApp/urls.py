7e17df5806a84573c7e9c8d1baf4df573eefe0f7
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-07 22:54:27 -0500

    feat # Proyecto Web X start
    Proyecto Web X start
    
    resolvesr: #
    se also: #

---
+++ Proyecto_Web_X/proyectoWebApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from proyectoWebApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+
+    path('', views.home, name="Home"),
+    path('servicios', views.servicios, name="Servicios"),
+    path('tienda', views.tienda, name="Tienda"),
+    path('blog', views.blog, name="Blog"),
+    path('contacto', views.contacto, name="Contacto"),
+
+]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
