6707f9a784cf73bbc98f514f7ce539b51109324a
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-09 10:54:31 -0500

    feat: Proyecto Web XII update
    Proyecto Web XII update
    
    resolve:
    see also:

--- Proyecto_Web_XII/proyectoWebApp/views.py
+++ Proyecto_Web_XII/proyectoWebApp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, "proyectoWebApp/home.html")
+
+def tienda(request):
+    return render(request, "proyectoWebApp/tienda.html")
+
+def blog(request):
+    return render(request, "proyectoWebApp/blog.html")
+
+def contacto(request):
+    return render(request, "proyectoWebApp/contacto.html")
