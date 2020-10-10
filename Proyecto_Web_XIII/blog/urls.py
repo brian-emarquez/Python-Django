9e02953ea492d999e60166e7017e85f8c3cc8106
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-10 12:15:31 -0500

    feat: Python Django Proyecto Web XIII complete Blog
    Python Django Proyecto Web XIII complete blog
    
    resolves:
    see also:

--- Proyecto_Web_XIII/blog/urls.py
+++ Proyecto_Web_XIII/blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
 
-    path('', views.blog, name="Blog"),
+    path("", views.blog, name="Blog"),
     
 
 ]
--- Proyecto_Web_XIII/blog/views.py
+++ Proyecto_Web_XIII/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
 
 def blog(request):
-    return render(request, "blog/blog.html")
+    
+    posts=Post.objects.all()
+    return render(request, "blog/blog.html",  {"posts":posts})
+
