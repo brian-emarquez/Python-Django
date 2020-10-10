fb53ac9c2802b2ac9da39cc64968b5679eac2574
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-10 15:37:58 -0500

    feat: Python Django Proyecto Web XV complete
    Python Django Proyecto Web XV complete
    
    resolves:
    see also:

--- Proyecto_Web_XV/blog/urls.py
+++ Proyecto_Web_XV/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     path("", views.blog, name="Blog"),
-    
+    path("categoria/<int:categoria_id>/", views.categoria, name="categoria"),
+
 
 ]
 
--- Proyecto_Web_XV/blog/views.py
+++ Proyecto_Web_XV/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Categoria
 
 
 # Create your views here.
@@ -9,3 +9,8 @@ def blog(request):
     posts=Post.objects.all()
     return render(request, "blog/blog.html",  {"posts":posts})
 
+def categoria(request, categoria_id):
+
+    categoria=Categoria.objects.get(id=categoria_id)
+    posts=Post.objects.filter(categorias=categoria)
+    return render(request, "blog/categoria.html", {'categoria':categoria, "posts":posts})
