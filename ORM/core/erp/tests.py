0125bd317fa5d1c7c1cdd4e3e02076e57bf00fb9
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-23 15:41:07 -0500

    ORM Update

--- ORM/core/erp/tests.py
+++ ORM/core/erp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from ORM.wsgi import *# importamos lo modulos de la carpeta wsgi.py
 
-# Create your tests here.
+from core.erp.models import Type
+
+# Listar
+
+#*********** HACER UNA CONSULTA ***********
+query = Type.objects.all()
+print(query) # muestra lo registro de la tabla Type
+
+#********* HACER UNA INSERIONN ***********
+t = Type()
+#t = Type(name = "Test 1")
+t.name = "Test 1"
+t.save()
