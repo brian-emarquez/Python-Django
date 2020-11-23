d8812946c69bd3e2b85da529d6feeecbcbc51b2e
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-23 16:15:43 -0500

    ORM II start

---
+++ ORM_II/core/erp/tests.py
@@ -0,0 +1,27 @@
+from ORM_II.wsgi import *# importamos lo modulos de la carpeta wsgi.py
+
+from core.erp.models import Type
+
+# Listar
+
+#*********** HACER UNA CONSULTA ***********
+query = Type.objects.all()
+print(query) # muestra lo registro de la tabla Type
+
+#********* HACER UNA INSERIONN ***********
+#t = Type()
+# #t = Type(name = "Test 1")
+# t.name = "Test 1"
+# t.save()
+
+#********* HACER UNA MODIFICACION ***********
+
+t = Type.objects.get(id=3) # Recuperacion de Objeto
+#print(t.name)
+t.name = "Test 2"
+t.save()
+
+#********* HACER UNA ELIMINACION ***********
+
+t = Type.objects.get(id=3) # Recuperacion de Objeto
+t.delete()
