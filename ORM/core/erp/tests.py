c555330d518f64b18a7e97157765163c18f4fc14
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-23 16:02:25 -0500

    ORM 1 Complete

--- ORM/core/erp/tests.py
+++ ORM/core/erp/tests.py
@@ -9,7 +9,19 @@ query = Type.objects.all()
 print(query) # muestra lo registro de la tabla Type
 
 #********* HACER UNA INSERIONN ***********
-t = Type()
-#t = Type(name = "Test 1")
-t.name = "Test 1"
+#t = Type()
+# #t = Type(name = "Test 1")
+# t.name = "Test 1"
+# t.save()
+
+#********* HACER UNA MODIFICACION ***********
+
+t = Type.objects.get(id=3) # Recuperacion de Objeto
+#print(t.name)
+t.name = "Test 2"
 t.save()
+
+#********* HACER UNA ELIMINACION ***********
+
+t = Type.objects.get(id=3) # Recuperacion de Objeto
+t.delete()
--- ORM/readme.md
+++ ORM/readme.md
@@ -1,3 +1,25 @@
-## Data Base Config
+# ORM
 
-![django](./images/db.png)
+## HACER UNA INSERIONN 
+```
+t = Type()
+# #t = Type(name = "Test 1")
+# t.name = "Test 1"
+# t.save()
+```
+
+## HACER UNA MODIFICACION
+
+```
+t = Type.objects.get(id=3) # Recuperacion de Objeto
+#print(t.name)
+t.name = "Test 2"
+t.save()
+```
+
+## HACER UNA ELIMINACION 
+
+```
+t = Type.objects.get(id=3) # Recuperacion de Objeto
+t.delete()
+```
