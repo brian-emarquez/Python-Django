d536ffd7bb796de84ea77be994174b809cb0ccbc
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-24 23:27:17 -0500

    feat Panel adminitrador complete
    panel adminitrador complete
    
    resolvesr: #
    se also: #

--- panel_administrador/core/erp/models.py
+++ panel_administrador/core/erp/models.py
@@ -6,8 +6,8 @@ class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
 
     def __str__(self):
-        return 'Nombre: {}'.format(self.name)
-
+        #return 'Nombre: {}'.format(self.name)
+        return self.name
     class Meta:
         verbose_name = 'Categoria'
         verbose_name_plural = 'Categorias'
