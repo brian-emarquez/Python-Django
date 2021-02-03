0c1f80993261d429832e2c2a8bf4f27f807300c3
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-03 12:30:56 -0500

    feat: Creando nuestro primer listado en django UPDATE 1
    Creando nuestro primer listado en django UPDATE 1
    
    Resolves: #
    See also: #

--- Creando_nuestro_primer_listado_en_django/core/erp/urls.py
+++ Creando_nuestro_primer_listado_en_django/core/erp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from core.erp.views import myfirstview, mysecondview
-
 app_name = 'erp'
 
 urlpatterns = [
-    path('uno/', myfirstview, name='vista1'),
-    path('dos/', mysecondview, name='vista2')
+    #path('uno/', myfirstview, name='vista1'),
+    #path('dos/', mysecondview, name='vista2')
 ]
