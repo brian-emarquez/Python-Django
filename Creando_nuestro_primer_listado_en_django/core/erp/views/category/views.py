57efbcae8843ebc80bc071ea0ecc4e9e0d460e8c
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-03 20:07:54 -0500

    Update 1

--- Creando_nuestro_primer_listado_en_django/core/erp/views/category/views.py
+++ Creando_nuestro_primer_listado_en_django/core/erp/views/category/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def category_list(request):
     data = {
         'title':'Listado de Categorias',
-        'categorias': Category.objects.all()
+        'categorias': category.objects.all()
     }
     return render(request, 'category/list.html', data)
