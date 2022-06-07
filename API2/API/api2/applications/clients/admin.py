39be0a191048476e3d8b95a10cf0bba330936593
Brian Marquez I <brian3marquez@gmail.com>
2022-06-07 13:30:05 +0000

    Delete Api

--- API2/API/api2/applications/clients/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Clientes, Addresses
-
-
-# Register your models here.
-admin.site.register(Clientes)
-admin.site.register(Addresses)
-
--- API2/API/api2/applications/clients/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Clientes(models.Model):
-    pass
-
-class Addresses(models.Model):
-    pass
